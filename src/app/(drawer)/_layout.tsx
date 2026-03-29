ad46abbf7303351e555e95255b2f70723883f8ef
José Jamerson <jjamersonmt@hotmail.com>
2026-03-29 15:14:10 -0300

    feat: inserido menu button

--- src/app/(drawer)/_layout.tsx
+++ src/app/(drawer)/_layout.tsx
@@ -2,34 +2,33 @@ import { Drawer } from "expo-router/drawer";
 import { Colors } from "../../constants/Colors";
 import { Platform, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { DrawerActions } from "@react-navigation/native";
 
 export default function DrawerLayout() {
   return (
     <Drawer
-      screenOptions={{
+      // Usamos uma função aqui para ganhar acesso ao objeto 'navigation' nativo
+      screenOptions={({ navigation }) => ({
         headerShown: true,
         headerTintColor: Colors.primary,
         drawerActiveTintColor: Colors.primary,
 
-        // CORREÇÃO PARA O ERRO 404 NA WEB:
-        // Substituímos o ícone do menu lateral que costuma dar erro 404 (.png)
-        headerLeft: (props) =>
-          Platform.OS === "web" ? (
-            <TouchableOpacity
-              onPress={props.onPress}
-              style={{ marginLeft: 15 }}
-            >
-              <Ionicons name="menu-outline" size={28} color={Colors.primary} />
-            </TouchableOpacity>
-          ) : undefined, // Mantém o ícone nativo no Android/iOS
-      }}
+        headerLeft: () => (
+          <TouchableOpacity
+            // DrawerActions.openDrawer() é a forma mais segura de disparar o evento
+            onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+            style={{ marginLeft: 15 }}
+          >
+            <Ionicons name="menu-outline" size={28} color={Colors.primary} />
+          </TouchableOpacity>
+        ),
+      })}
     >
       <Drawer.Screen
         name="dashboard"
         options={{
           drawerLabel: "Início",
           title: "JJ Banking",
-          // Garantimos que o ícone do item no menu lateral também seja estável
           drawerIcon: ({ color, size }) => (
             <Ionicons name="home-outline" size={size} color={color} />
           ),
